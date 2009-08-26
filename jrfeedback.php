34c10dc5994982eb577a8e000f72c377fcc8b2af
josephwardell <josephwardell_github@old-jewel.com>
2009-08-26 08:37:14 -0700

    added sender name in message body and a timestamp to message body and subject

--- jrfeedback.php
+++ jrfeedback.php
@@ -19,28 +19,43 @@ if (array_key_exists('feedback', $_REQUEST)) {
 	// suppressed@auma.pair.com or anonymous@auma.pair.com
 	// FogBugz would try to send them an email and generate another 
 	// ticket telling me "Undelivered Mail Returned to Sender"
-	
-	// Check for well formatted email address, if it's OK use their address in the "from" header, else
-	// use generic support address
-	if (eregi('^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$', $_REQUEST['email'])) {
+	// TODO: It'd be nice to make sure the string looks like a 
+	// real email address but I'll assume everyone is playing nice.
+	if ($_REQUEST['email'] != '') {
 	    $email = $_REQUEST['email'];
-	} else {    
-        $email = 'YOUR_EMAIL_ADDRESS_HERE@gmail.com';
+	} else {
+        $email = 'feedback_test_noreturnaddressgiven@old-jewel.com';
     }
 	$feedback = $_REQUEST['feedback'];
 	$bundleID = $_REQUEST['bundleID'];
 	$systemProfile = $_REQUEST['systemProfile'];
-	
+
+	if ($_REQUEST['name'] != '') {
+	    $senderName = $_REQUEST['name'];
+	} else {
+        $senderName = '(user chose to not give name)';
+    }
+
 	$headers = 'From: ' . $email . "\r\n" .
         'Reply-To: ' . $email . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
 	
+	$t = date("n\/j\/Y  H:i:s");
+
 	$msg .= "$feedback\n";
+
+	// include user's name in message body
+	$msg .= "\n--------\n";
+	$msg .= "\nfrom $senderName on $t\n";
 	$msg .= "\n--------\n\n";
+
 	$msg .= "Bundle ID: $bundleID\n";
 	$msg .= "System Profile: $systemProfile\n";
 	
-	mail("YOUR_FEEDBACK_EMAIL_ADDRESS_HERE@gmail.com", "[$feedbackType] $appName $appVersion", $msg, $headers);
+	// include a timestamp in the subject so that each report has a unique subject
+	// this way, Mail.app will see each message as the beginning of a new message thread
+	// making followup easier
+	mail("feedback@old-jewel.com", "[$feedbackType] $appName $appVersion ($t)", $msg, $headers);
 }
 ?>
 
